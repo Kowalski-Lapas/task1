c7394baffe77f9b34ded728f80d80991a91a7a2c
user07 <user07>
2023-10-05 14:12:16 +0300

    gjmjfg

---
+++ app/src/main/java/com/example/myapplication978789789/Onboard1Activity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication978789789;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Onboard1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboard1);
+    }
+}
